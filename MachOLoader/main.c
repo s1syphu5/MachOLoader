244d8a7aae344af50d613511ff1c6167b774b6ee
s1syphu5 <a@gmail.com>
2020-06-08 19:45:08 +0300

    Initial Commit

---
+++ MachOLoader/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MachOLoader
+//
+//  Created by s1syphu5 on 08/06/2020.
+//  Copyright © 2020 s1syphu5. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
